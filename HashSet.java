f2a33fdcc474a7a8f1edc020c609851170edd1c3
SHAH FAHAD <47359778+shahfahad3@users.noreply.github.com>
2020-06-26 00:08:04 +0530

    https://www.hackerrank.com/challenges/java-hashset/problem

---
+++ HashSet.java
@@ -0,0 +1,59 @@
+
+
+Sample Input
+
+5
+john tom
+john mary
+john tom
+mary anna
+mary anna
+Sample Output
+
+1
+2
+2
+3
+3
+Explanation
+
+After taking the first input, you have only one pair: (john,tom)
+After taking the second input, you have two pairs: (john, tom) and (john, mary)
+After taking the third input, you still have two unique pairs.
+After taking the fourth input, you have three unique pairs: (john,tom), (john, mary) and (mary, anna)
+After taking the fifth input, you still have three unique pairs.
+
+
+
+
+//CODE
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+//Write your code here
+Set<String> st=new HashSet<>();
+for(int i=0;i<t;i++)
+{
+    String s1=pair_left[i]+" "+pair_right[i];
+    st.add(s1);
+    System.out.println(st.size());
+}
+
+    }
+}
